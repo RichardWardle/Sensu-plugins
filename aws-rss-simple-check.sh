5c4ba6634cd71c4eb700a3ba0e46eef2a78a9fce
RichardWardle <wardle.rich@gmail.com>
2017-07-20 15:59:59 +1000

    Update and rename aws-rss-simple-check-WIP.sh to aws-rss-simple-check.sh

---
+++ aws-rss-simple-check.sh
@@ -0,0 +1,38 @@
+date=$(date +%a," "%d" "%b" "%Y)
+
+if [[ $1 == "" ]] || [[ $2 == "" ]] || [[ $# != 2 ]]; then
+        echo "ERROR: Usage: $1 = service e.g elasticsearch $2 = zone e.g. us-east-1"
+        echo "This would call http://status.aws.amazon.com/rss/elasticsearch-us-east-1.rss"
+        echo "The RSS links can be found: http://status.aws.amazon.com/"
+        exit 3
+fi
+
+service="$1"
+zone="$2"
+link="http://status.aws.amazon.com/rss/${service}-${zone}.rss"
+
+curl -H "Content-Type: text/xml" -N -s -g $link | xmlstarlet sel -t -m "/rss/channel/item" -v "title" -o "|" -v "pubDate" -o "|" -v "description" -n 1> output.txt
+
+if [[ $? != 0 ]]; then
+        echo "I had a problem access: , please check internet connectivity, your paramters and any other connectivity issues"
+        exit 3
+fi
+
+grep "$date" output.txt > results.txt
+
+if [[ $(wc -l results.txt | cut -f 1 -d ' ') == 0 ]]; then
+        echo "OK: There are no reported problems for $service-$zone-sz on $date, The last reported issue or update was:"
+        echo ""
+        tail -n 1 output.txt | awk -F "|" '{printf "    Date Post: " $2 "\n    Issue Title: " $1 "\n    Description: " $3 "\n"}'
+        rm results.txt output.txt
+        exit 0
+fi
+
+echo "WARNING: There are the reported issues for $service-$zone-sz on $date on RSS feed: $link"
+echo ""
+tac results.txt | while read LINE; do
+        awk -F "|" '{printf "Date Post: " $2 "\nIssue Title: " $1 "\nDescription: " $3 "\n\n"}'
+done
+
+rm results.txt output.txt
+exit 1
